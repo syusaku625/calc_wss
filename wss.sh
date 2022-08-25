b818d0fbfe706f60d2f5ec83548df05d707ebba3
syusaku625 <maedasyu625@gmail.com>
2022-08-25 17:12:55 +0900

    add shell script

--- wss.sh
+++ wss.sh
@@ -1,10 +1,27 @@
-./Main NPH_S12 342
-./Main NPH_S18 289
-./Main NPH_S23 436
-./Main NPH_S24 324
-./Main NPH_S25 324
-./Main NPH_S26 311
-./Main NPH_S31 347
-#./Main NPH_S33 320
-./Main NPH_S35 393
-./Main NPH_S38 292
+./Main input_data/Normal_S10 Normal_S10 406
+./Main input_data/Normal_S11 Normal_S11 320
+./Main input_data/Normal_S15 Normal_S15 347
+./Main input_data/Normal_S18 Normal_S18 342
+./Main input_data/Normal_S19 Normal_S19 375
+./Main input_data/Normal_S20 Normal_S20 320
+./Main input_data/NPH_S12 NPH_S12 342
+./Main input_data/NPH_S18 NPH_S18 289
+./Main input_data/NPH_S23 NPH_S23 436
+./Main input_data/NPH_S24 NPH_S24 324
+./Main input_data/NPH_S25 NPH_S25 324
+./Main input_data/NPH_S26 NPH_S26 311
+./Main input_data/NPH_S31 NPH_S31 347
+./Main input_data/NPH_S33 NPH_S33 328
+./Main input_data/NPH_S35 NPH_S35 393
+./Main input_data/NPH_S38 NPH_S38 292
+
+#./Main NPH_S12 342
+#./Main NPH_S18 289
+#./Main NPH_S23 436
+#./Main NPH_S24 324
+#./Main NPH_S25 324
+#./Main NPH_S26 311
+#./Main NPH_S31 347
+##./Main NPH_S33 320
+#./Main NPH_S35 393
+#./Main NPH_S38 292
